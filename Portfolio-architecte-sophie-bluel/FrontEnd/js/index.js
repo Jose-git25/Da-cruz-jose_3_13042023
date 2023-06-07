1fac7936a5dad68e8c4a9ad79618e5c6e94e1a59
Jose-git25 <dragaofelina@gmail.com>
2023-06-07 17:03:43 +0200

    Works

--- Portfolio-architecte-sophie-bluel/FrontEnd/js/index.js
+++ Portfolio-architecte-sophie-bluel/FrontEnd/js/index.js
@@ -1,11 +1,34 @@
 console.log('hello world')
 
-fetch ('http://localhost:5678/api-docs/')
-.then( data=> {
-    return data.json()
-}).then(json=>{
-    console.log(json)
-})
- Get ('http://localhost:5678/api/works')
- Get ('http://localhost:5678/api/categories')
+const apiWorks = async () => {
+    try {
+        await fetch ("http://localhost:5678/api/works" ,{
+            method: "GET",
+            headers: {
+              Accept: "application/json",  
+            },
+
+        })
+        .then((response) => response.json())
+        .then((WorksResponse)) ; {
+            works=WorksResponse;     
+        };
+    } catch (error)  {
+     console.log("Erreur dans la récupération des données :", error)   
+    }     
+
+};    
+// }
+// fetch ('http://localhost:5678/api-docs/')
+// .then( data=> {
+//     return data.json()
+// }).then(json=>{
+//     console.log(json)
+// })
+// //  get ('http://localhost:5678/api/works');
+// //  get ('http://localhost:5678/api/categories');
+//  fetch ('http://localhost:5678/api/works');{
+//     get/works
+//  }
+ 
  
