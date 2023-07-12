3dc5075fc5b1e948f8e284c00f9aa42ba3318fca
Jose-git25 <dragaofelina@gmail.com>
2023-07-12 15:19:53 +0200

    Update index.js

--- Portfolio-architecte-sophie-bluel/FrontEnd/js/index.js
+++ Portfolio-architecte-sophie-bluel/FrontEnd/js/index.js
@@ -54,12 +54,14 @@ fetch("http://localhost:5678/api/categories", {
 
     categories.forEach((categorie) => {
       console.log(categorie);
+      console.log(categorie.name);
+      // let nouvelleDiv = document.createElement("div ");
+      // let div = document.createElement("button");
+
+      // button.value = categories.name;
+      // nouvelleDiv.appendChild(button);
+
+      // console.log(button);
     });
     // Set.forEach = "Objets,Appartements,Hotels & restaurants";
-
-    // let nouvelleDiv = document.createElement("div ");
-    let button = document.createElement("button");
-    button.value = categories;
-
-    button.appendChild;
   });
