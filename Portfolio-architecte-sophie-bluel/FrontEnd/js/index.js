27202f31ef4d452e8e632ff05a22521949d87c1a
Jose-git25 <dragaofelina@gmail.com>
2023-07-12 16:31:54 +0200

    Update index.js

--- Portfolio-architecte-sophie-bluel/FrontEnd/js/index.js
+++ Portfolio-architecte-sophie-bluel/FrontEnd/js/index.js
@@ -55,7 +55,9 @@ fetch("http://localhost:5678/api/categories", {
     categories.forEach((categorie) => {
       console.log(categorie);
       console.log(categorie.name);
-
+      let nouvelleDiv = document.createElement("div");
+      div = document.createElement("filtres");
+      div = document.createElement("button");
       let button = document.createElement("button");
       button.type = "button";
       button.textContent = categorie.name;
